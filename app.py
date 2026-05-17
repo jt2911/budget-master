55d4619bbf611917b44e5facd9786b0b204e193f
jt <wongjt2006@gmail.com>
2026-05-17 21:56:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -564,5 +564,5 @@ def chart_data():
         'monthly': monthly_totals
     })
 # ============== 运行应用 ==============
-if __name__ == '__main__':
-  app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run()
