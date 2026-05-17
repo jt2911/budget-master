2a050b8901881150ac583aa31910eeb642f887a7
jt <wongjt2006@gmail.com>
2026-05-17 21:31:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'your@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your-app-password'  # Gmail App Password, not login password
+app.config['omxj uxbj xqul kxcv'] = 'your-app-password'  # Gmail App Password, not login password
 app.config['MAIL_DEFAULT_SENDER'] = 'your@gmail.com'
 
 mail = Mail(app)
